52a6b1525ee68aa2523bfe6c3090ae75980a4fee
Carlos Murillo Blanco <carlos.murillo@digitalvirgo.com>
2023-07-10 12:33:34 +0200

    Modificado el archivo vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  root: './frontend', // ajusta esto a la ubicación correcta
+  base: './', // ruta base para los import
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // directorio de salida
+    emptyOutDir: true, // limpiar el directorio de salida antes de construir
+  },
 })
