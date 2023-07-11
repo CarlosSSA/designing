b45cf196af93b4c824c675769b190c6bc2559a62
Carlos Murillo Blanco <carlos.murillo@digitalvirgo.com>
2023-07-11 22:02:15 +0200

    añadidos unos cuantos console.logs

--- frontend/src/apis/authApi.js
+++ frontend/src/apis/authApi.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-console.log(`la variable de enterno de VITE_BACKEND_URL es LOL XD ${import.meta.env.VITE_PROD_URL}`)
+console.log(`la variable de enterno de VITE_BACKEND_URL es LOL XD ${import.meta.env.VITE_BACKEND_URL}`)
+console.log(`el modo en que se ejecuta la aplicacion es VITE_BACKEND_URL es LOL XD ${import.meta.env.MODE}`)
+console.log(`la base url es VITE_BACKEND_URL es LOL XD ${import.meta.env.BASE_URL}`)
+console.log(`si la aplicacion se ejecuta en produccion de VITE_BACKEND_URL es LOL XD ${import.meta.env.PROD}`)
+console.log(`si la aplicacion se ejecuta en desarrollo de VITE_BACKEND_URL es LOL XD ${import.meta.env.DEV}`)
+console.log(`si la aplicacion se ejecuta en el servidor de VITE_BACKEND_URL es LOL XD ${import.meta.env.SSR}`)
+
+
 const recetaApi = axios.create({
     //baseURL:import.meta.env.VITE_API_URL 
    // baseURL:'http://localhost:4001/api/'  si lo pongo así funciona
