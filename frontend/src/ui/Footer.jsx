12ed5e32f24ac92f8edbd842774dc47f302e1d6d
Carlos Murillo Blanco <carlos.murillo@digitalvirgo.com>
2023-10-06 17:47:39 +0200

    El footer ahora cambia si está en receta

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -6,10 +7,16 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import AddIcon from '@mui/icons-material/Add';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import ExploreIcon from '@mui/icons-material/Explore';
+import SaveIcon from '@mui/icons-material/Save';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import Paper from '@mui/material/Paper';
 
 export default function Footer() {
   const [value, setValue] = React.useState(0);
+  const location = useLocation();
+
+  // Verificar si la URL contiene "/recipe/"
+  const isRecipePage = location.pathname.includes('/recipe/');
 
   return (
     <Box sx={{ pb: 7 }}>
@@ -21,9 +28,21 @@ export default function Footer() {
             setValue(newValue);
           }}
         >
-          <BottomNavigationAction icon={<AddIcon />} />
-          <BottomNavigationAction icon={<CalendarTodayIcon />} />
-          <BottomNavigationAction icon={<ExploreIcon />} />
+          {/* Si estamos en una página de receta, mostramos estos botones */}
+          {isRecipePage ? (
+            <>
+              <BottomNavigationAction label="Guardar" icon={<SaveIcon />} />
+              <BottomNavigationAction label="Likear" icon={<ThumbUpIcon />} />
+              <BottomNavigationAction label="Calendario" icon={<CalendarTodayIcon />} />
+            </>
+          ) : (
+            // Si no, mostramos estos otros botones
+            <>
+              <BottomNavigationAction icon={<AddIcon />} />
+              <BottomNavigationAction icon={<CalendarTodayIcon />} />
+              <BottomNavigationAction icon={<ExploreIcon />} />
+            </>
+          )}
         </BottomNavigation>
       </Paper>
     </Box>
